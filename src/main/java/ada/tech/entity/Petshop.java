f38505ef0b04230d2a88add458bbfdb6e26dd03f
izuca <adriel200010@hotmail.com>
2025-08-30 00:09:30 -0300

    add initial setup for Petshop application with Docker, REST endpoints, and entity models

--- src/main/java/ada/tech/entity/Petshop.java
+++ src/main/java/ada/tech/entity/Petshop.java
@@ -1,9 +1,15 @@
 package ada.tech.entity;
 
+
 import jakarta.persistence.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public class Petshop {
+    private static List<Produto> estoque;
+    private static List<Produto> carrinho;
 
 }
